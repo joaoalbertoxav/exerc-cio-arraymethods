1393bc82a0ef420a8f7ba17a48b7412a44505f65
joaoalbertoxav <127453239+joaoalbertoxav@users.noreply.github.com>
2024-04-10 20:49:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//01 - Converter um array em uma string com toString
+
+//let frutas = ["maçã","banana","laranja","uva"].toString();
+//console.log(frutas)
+
+
+//02 - Unir os elementos de um array em uma string com join
+
+//let carros = ["Corolla","Fiat","Mercedes","Chevrolet"].join(" - ");
+//console.log(carros)
+
+
+//03 - Remover o último elemento de um array com pop
+
+//let objt = ["boneco", "carro", "parafuso", "pneu"];
+//let = objt.pop()
+//console.log(objt)
+
+
+//04 - Adicionar um elemento ao final de um array com push
+
+//let compras = ["sapato", "mochila", "meias"]
+//let = compras.push("blusas","calças")
+//console.log(compras)
+
+
+//05 - Inserir elementos em uma determinada posição do array com o método splice (Note:primeiro parâmetro é a posição,
+// o segundo parâmetro é a quantidade de elementos a serem removidos, e o terceiro parametro é o elemtento em si)
+
+//let frutas = ["maçã", "morango", "limão", "pêssego"]
+//let = frutas.splice(0,2, "banana", "kiwi")
+//console.log(frutas) 
+
+
+//06 - Você tem uma lista de compras ["Banana", "Orange", "Apple", "Mango","Grape","Pitomba"]
+// e necessita remover o ultimo elemento e mostrar a nova lista de compras.
+
+//let compras =  ["Banana", "Orange", "Apple", "Mango","Grape","Pitomba"]
+//let = compras.pop();
+//console.log(compras)
+
+
+//07 - Você acidentalmente esqueceu qual que era o ultimo elemento que 
+//você removeu e gostaria de mostrar no console qual que era
+
+//let deposito = ["parafusos", "fita isolante", "spray", "lanterna"];
+//let = deposito.push("lata");
+//console.log(deposito)
+
+
+//08 -Você precisa unir a lista de frutas com 
+//a lista de limpeza ["Veja", "Detergente", "Esponja", "Sabao em po"] e ["Veja", "Detergente", "Esponja", "Sabao em po"] 
+
